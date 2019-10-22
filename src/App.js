6752d0394031708c93ce2446250cbbbb0acb9fe4
정연수 <jys1015@hanyang.co.kr>
2019-10-22 16:02:51 +0900

    3.Error Boundaries Comcept

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React, { Component, Fragment } from 'react';
 import {createPortal} from "react-dom";
 
+// Portal의 에러를 발견할 수 없다.
+class ErrorMaker extends Component {
+  state = {
+    friends: ["jisu", "flynn", "daal", "kneeprayer"]
+  };
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        friends: undefined
+      });
+    }, 2000);
+  };
+  render() {
+    const { friends } = this.state;
+    return friends.map(friend => ` ${friend} `);
+  }
+}
+
+
 //Portal을 이용하여 React의 scope(root)밖에서도 수정을 할 수 있다.
 class Portals extends Component{
   render(){
@@ -19,13 +38,27 @@ class ReturnTypes extends Component{
   }
 }
 
+const ErrorFallback = () => "Sorry something went wrong"
 class App extends Component {
+  state = {
+    hasError:false
+  };
+  
+  componentDidMount = (error, info) => {
+    console.log(`catched ${error} and error infomation = ${JSON.stringify(info)}`);
+    this.setState({
+      hasError:true
+    });
+  };
+
   render(){ 
+    const {hasError} = this.state
     return (
       //Fragment를 사용하여 기존의 방식인 Array와 span으로 묶어서 할 필요가없다.
       <Fragment>
         <ReturnTypes />
         <Portals/>
+        {hasError ? <ErrorFallback/> : <ErrorMaker />}
       </Fragment>
     )
   }
