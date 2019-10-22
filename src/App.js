d41af7c6eff724a93eb2d6aa933042ceddf0780c
정연수 <jys1015@hanyang.co.kr>
2019-10-22 15:42:20 +0900

    1. Return Types Strings and Fragments

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
+class ReturnTypes extends Component{
+  render(){
+    return "hello";
+  }
+}
 class App extends Component {
   render(){ 
-    return <div className="App"/>
+    return (
+      //Fragment를 사용하여 기존의 방식인 Array와 span으로 묶어서 할 필요가없다.
+      <Fragment>
+        <ReturnTypes />
+      </Fragment>
+    )
   }
 }
 
